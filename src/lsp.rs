b4a6b36f8dccb55018ba338dc327d728d149d9e3
Mathieu Sabourin <mathieu.c.sabourin@gmail.com>
2023-11-05 14:07:12 -0700

    [feat] Handle Initialize request.

--- src/lsp.rs
+++ src/lsp.rs
@@ -1,13 +1,48 @@
-use serde_json::json;
-use crate::jsonrpc::Request;
+use serde_json::{Value};
+use serde::Deserialize;
+use lsp_types::{lsp_request, InitializeResult, ServerInfo, ServerCapabilities};
+use lsp_types::request::Request as LSPRequest;
 
-pub fn get_response(req: Request) -> String {
-    let body = json!({
-        "id": 1,
-        "result": {
-            "all": "good"
-        }
-    }).to_string();
+use crate::jsonrpc::Request;
+use crate::utils::err_msg;
+
+pub fn get_response(jsonrpc_req: Request) -> String {
+    let json_req: Value = serde_json::from_str(&jsonrpc_req.body).unwrap();
+    let method = json_req["method"].as_str().unwrap();
+
+    let b = match method {
+        <lsp_request!("initialize")>::METHOD => {
+            let params =
+                match <lsp_request!("initialize") as LSPRequest>::Params::deserialize(
+                    &json_req["params"]
+                 ) {
+                    Ok(initParams) => initParams,
+                    Err(err) => {
+                        err_msg(format!("{err}"));
+                        panic!();
+                    },
+                };
+
+            serde_json::json!({
+                "id": 1,
+                "result": InitializeResult {
+                    capabilities: ServerCapabilities::default(),
+                    server_info: Some(ServerInfo {
+                        name: String::from("bash-lsp"),
+                        version: Some(String::from("0.0.1")),
+                    }),
+                }
+            })
+        },
+        _ => serde_json::json!({
+            "id": 1,
+            "result": {
+                "all": method,
+            }
+        }),
+    };
+
+    let body = b.to_string();
     let len = body.len();
 
     return format!("Content-Length: {len}\r\n\r\n{body}")
